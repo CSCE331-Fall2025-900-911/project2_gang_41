8f860328f75e7b9b6501b21d67ab7392ba735b33
jasonlau05 <jasonlaucr@gmail.com>
2025-10-01 17:43:24 -0500

    updated generation

--- orderHistoryGenerator.py
+++ orderHistoryGenerator.py
@@ -11,12 +11,10 @@ customers = pd.read_csv("customers_data.csv")
 employees = pd.read_csv("employee_data.csv")
 
 data = []
-num_orders = 2000 # change me for more orders
+num_orders = 160000 # change me for more orders
 
 end_date = datetime.today()
-start_date = end_date - timedelta(weeks=52) # past 52 weeks
-
-# generate random dates and sort from startdate to enddate
+start_date = end_date - timedelta(weeks=52)
 order_dates = [fake.date_time_between(start_date=start_date, end_date=end_date) for _ in range(num_orders)]
 order_dates.sort()
 
@@ -24,16 +22,15 @@ order_id = 1
 
 for order_date in order_dates:
     customer = customers.sample(1).iloc[0]
-    # we do not accept blowjobs as payment
     payment_method = random.choice(["cash", "card", "bitcoin", "Turkish Lira", "Japanese Yen", "TSLA stock", "10-year US Series EE Savings Bonds Non-Marketable", "Klarna", "stole it"])
 
-    # weight so most ppl order 1 drink
+    # weight so most ppl o
     num_items = random.choices([1, 2, 3], weights=[90, 5, 5], k=1)[0]
     chosen_items = menu_items.sample(num_items)
 
     for _, menu_item in chosen_items.iterrows():
-        # weight again for quantity
-        quantity = random.choices([1, 2], weights=[95, 5], k=1)[0]
+        # weight so most ppl order 1 drink
+        quantity = random.choices([1, 2], weights=[90, 10], k=1)[0]
         price = float(menu_item["COST"])
         total = round(price * quantity, 2)
         random_number = random.randint(1, 20)
@@ -51,11 +48,11 @@ for order_date in order_dates:
             "TotalPrice": total
         })
 
-        order_item_id += 1
+        # order_item_id += 1
 
     order_id += 1
 
-# find order_final.csv in files
+# Save CSV
 df = pd.DataFrame(data)
 df.to_csv("order_final.csv", index=False)
 
